81a2784a513cde4483de0b3d14f5e37fc27bc9ae
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-01-25 09:46:21 +0000

    Create nosleepdeobf.js

---
+++ RiceTeaCatPanda/Web/No Sleep/nosleepdeobf.js
@@ -0,0 +1,30 @@
+let _0x1d8e = ['gamerfuel=Jan\x2027,\x202020\x2004:20:00', 'Jan\x2027,\x202020\x2004:20:00', 'getTime', 'exec', 'floor', 'getElementById', 'gamer\x20timer', 'AES', 'decrypt', 'U2FsdGVkX18kRm6FDkRVQfVuNPTxyOnJzpu8QnI/9UKoCXp6hQcley11nBnLIItj', 'ok\x20boomer', 'innerHTML', 'Utf8', 'cookie'];
+
+(function (_0x29eed8, _0x4bb4aa) {
+    var _0x47e29c = function (_0x2d3fd2) {
+        while (--_0x2d3fd2) {
+            _0x29eed8['push'](_0x29eed8['shift']());
+        }
+    };
+    _0x47e29c(++_0x4bb4aa);
+}(_0x1d8e, 0x99));
+
+const toName = function (_0x545e19, _0x47cdd3) {
+    _0x545e19 = _0x545e19 - 0x0;
+    return _0x1d8e[_0x545e19];
+};
+
+console.log('0: ' + toName('0x0'));
+console.log('1: ' + toName('0x1'));
+console.log('2: ' + toName('0x2'));
+console.log('3: ' + toName('0x3'));
+console.log('4: ' + toName('0x4'));
+console.log('5: ' + toName('0x5'));
+console.log('6: ' + toName('0x6'));
+console.log('7: ' + toName('0x7'));
+console.log('8: ' + toName('0x8'));
+console.log('9: ' + toName('0x9'));
+console.log('a: ' + toName('0xa'));
+console.log('b: ' + toName('0xb'));
+console.log('c: ' + toName('0xc'));
+console.log('d: ' + toName('0xd'));
